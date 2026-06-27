3a993be5a48aeac7488b5d5a36e3e26bd6621b17
abubakarafzal0101 <abubakarafzal0101@gmail.com>
2026-06-27 17:55:25 +0500

    issue resolve in routes of projects

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
 import upload from "../middleware/multer.js";
-import { addProject } from "../controllers/project.controllers.js";
+import {
+  addProject,
+  deleteProject,
+  GetAdminProjects,
+} from "../controllers/project.controllers.js";
 const projectRouter = express.Router();
 
 projectRouter.post("/add", isAuth, upload.single("projectImage"), addProject);
-projectRouter.delete("/delete/:projectId", isAuth, addProject);
-projectRouter.get("/get-admin-projects", isAuth, addProject);
+projectRouter.delete("/delete/:projectId", isAuth, deleteProject);
+projectRouter.get("/get-admin-projects", isAuth, GetAdminProjects);
 
 export default projectRouter;
